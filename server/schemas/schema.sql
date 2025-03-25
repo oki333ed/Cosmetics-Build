94782072e1896c4ffe5717c3a948d4c873e83368
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2025-03-25 15:11:00 -0400

    initial commit

---
+++ server/schemas/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS Cosmetics (
+    cosmeticID INTEGER PRIMARY KEY,
+    cosmeticName TEXT,
+    cosmeticAmount INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS Accounts (
+    accountID INTEGER PRIMARY KEY, 
+    creditsAmount INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS AccountCosmetics (
+    accountID INTEGER PRIMARY KEY,
+    cosmeticID INTEGER,
+    isActive INTEGER
+);
+
+INSERT INTO Cosmetics (cosmeticID, cosmeticName, cosmeticAmount) VALUES 
+    (1001, "Test Hat", 1000), 
+    (2001, "Test Mask", 1000), 
+    (3001, "Test Object", 1000),
+    (4001, "Test Particle Effect", 1000);
---
+++ server/src/index.ts
@@ -0,0 +1,101 @@
+export interface Env {
+	DB: D1Database;
+}
+
+async function readRequestBody(request: Request) {
+	return JSON.stringify(await request.json());
+}
+
+export default {
+	async fetch(request, env): Promise<Response> {
+		const {pathname} = new URL(request.url);
+		switch(pathname) {
+			case "/api/addCosmeticToUser": {
+				const error = '{"error": 400}'
+				if (request.method !== "POST") {
+					return Response.json(JSON.parse(error))
+				}
+
+				const reqBody = await readRequestBody(request);
+				const {results} = await env.DB.prepare(
+					"SELECT * FROM Accounts WHERE accountID = ?"
+				)
+				.bind(JSON.parse(reqBody)["accountID"])
+				.all();
+
+				if (results.length !== 0) {
+					return Response.json(JSON.parse(error))
+				} else {
+					await env.DB.prepare(
+						"INSERT INTO AccountCosmetics (accountID, cosmeticID, isActive) VALUES (?, ?, ?)"
+					)
+					.bind(JSON.parse(reqBody)["accountID"])
+					.run();
+
+					const code = '{"code": 200}'
+					return Response.json(JSON.parse(code));
+				}
+			}
+
+			case "/api/cosmetics": {
+				const {results} = await env.DB.prepare(
+					"SELECT * FROM Cosmetics"
+				)
+				.all();
+	
+				return Response.json(results);
+			}
+
+			case "/api/cosmeticsForUser": {
+				const {results} = await env.DB.prepare(
+					"SELECT * FROM AccountCosmetics WHERE accountID = ?"
+				)
+				.bind(new URL(request.url).searchParams.get("accountID"))
+				.all();
+
+				return Response.json(results);
+			}
+
+			case "/api/createUser": {
+				const error = '{"error": 400}'
+				if (request.method !== "POST") {
+					return Response.json(JSON.parse(error))
+				}
+
+				const reqBody = await readRequestBody(request);
+				const {results} = await env.DB.prepare(
+					"SELECT * FROM Accounts WHERE accountID = ?"
+				)
+				.bind(JSON.parse(reqBody)["accountID"])
+				.all();
+
+				if (results.length !== 0) {
+					return Response.json(JSON.parse(error))
+				} else {
+					await env.DB.prepare(
+						"INSERT INTO Accounts (accountID, creditsAmount) VALUES (?, 0)"
+					)
+					.bind(JSON.parse(reqBody)["accountID"])
+					.run();
+
+					const code = '{"code": 200}'
+					return Response.json(JSON.parse(code));
+				}
+			}
+
+			case "/api/getAccount": {
+				const {results} = await env.DB.prepare(
+					"SELECT * FROM Accounts WHERE accountID = ?"
+				)
+				.bind(new URL(request.url).searchParams.get("accountID"))
+				.all();
+	
+				return Response.json(results);
+			}
+		}
+
+		return new Response (
+			"hello there :3",
+		);
+	},
+} satisfies ExportedHandler<Env>;
